44e239cac514d291cfe0f3dc0ad8e3f1804b8be1
Zeucny <denis.crenn@isen-bretagne.fr>
2016-05-20 09:16:15 +0200

    ico + style

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 	?>
 	<head>
 		<title>VisitISEN</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="shortcut icon" type="image/x-icon" href="img/isen.ico" />
 	</head>
 	<body>
 
