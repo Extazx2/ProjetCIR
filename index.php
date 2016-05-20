2cbc21cc23e184f2369c2bdf380e659e08eca9ea
Zeucny <denis.crenn@isen-bretagne.fr>
2016-05-20 09:30:36 +0200

    image étage

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 		<link rel="shortcut icon" type="image/x-icon" href="img/isen.ico" />
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 	</head>
+
 	<body>
+
 	</body>
 </html>
