7312ec7d173898ebf7e02f8d377d1a336b164f0e
Zeucny <denis.crenn@isen-bretagne.fr>
2016-05-20 09:21:17 +0200

    jquery

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 		<title>VisitISEN</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<link rel="shortcut icon" type="image/x-icon" href="img/isen.ico" />
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 	</head>
 	<body>
-
 	</body>
 </html>
