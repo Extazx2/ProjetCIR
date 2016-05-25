07d99e8d0ea71bb18019128885d7ddc396dc88df
Zeucny <denis.crenn@isen-bretagne.fr>
2016-05-25 08:22:33 +0200

    welcome

---
+++ view/welcome.php
@@ -0,0 +1,9 @@
+<?php
+echo '
+		<section class="accueil">
+			<h1>VisitISEN</h1>
+			<h3>Qui êtes-vous ?</h3>
+			<button class="ambassadeur">Ambassadeur</button>
+			<button class="visiteur">Visiteur</button>
+		</section>';
+?>
