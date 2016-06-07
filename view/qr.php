799207286a03841c9789d520be84fc8d909a0451
Zeucny <denis.crenn@isen-bretagne.fr>
2016-06-07 14:07:46 +0200

    qr

--- view/qr.php
+++ view/qr.php
@@ -4,14 +4,18 @@
     	<div class="row"></div>
 			<div class="row">
 				<a class="col s12 waves-effect waves-light btn red red lighten-1">Scannez un QRCode à proximité</a>
-			</div>
-	</div>
+			</div></div>
     <video autoplay="" id="video"></video>
     <script>var video = document.getElementById(\'video\');
 
     QCodeDecoder().decodeFromCamera(video, function(er,res){
-      .load("index.php?etage=1&amp;arrivee='.$_GET['arrivee'].'$amp;depart=" + res)
+      console.log(res);
     });</script>
 ';
+  //.load("index.php?etage=1&amp;arrivee='.$_GET['arrivee'].'$amp;depart=" + res);
+
+/*
+
+*/
 
 ?>
