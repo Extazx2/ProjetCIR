6ae5e637f263d94befdf81f1f04295951c9505df
Extazx2 <touin.marcantoine@gmail.com>
2016-06-07 14:18:34 +0200

    w3c validation

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 
 	<body>
 
-	  <nav class="red lighten-1" role="navigation">
+	  <nav class="red lighten-1">
 	    <div class="nav-wrapper container"><a id="logo-container" href="index.php" class="brand-logo">VISITISEN</a>
 	      <ul class="right hide-on-med-and-down">
 	        <li><a href="index.php">Accueil</a></li>
