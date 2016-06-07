cc9035d04d2e6dafb3f02ddf8878d4917e3a3038
Extazx2 <touin.marcantoine@gmail.com>
2016-06-07 17:10:23 +0200

    edit style

--- view/qr.php
+++ view/qr.php
@@ -1,16 +1,17 @@
 <?php
     echo '<div class="container">
-    	<div class="row"></div>
+    		<div class="row"></div>
 			<div class="row">
 				<a class="col s12 waves-effect waves-light btn red red lighten-1">Scannez un QRCode à proximité</a>
-			</div></div>
-	<div id="reader" style="width:300px;height:250px">
-	</div>
+			</div>
+		</div>
+		<div class="container">
+			<div id="reader" class="col s12">
+			</div>
+		</div>
+
     <script> $(\'#reader\').html5_qrcode(function(data){
          location.href = "index.php?etage=1&amp;arrivee='.$_GET['arrivee'].'$amp;depart=" + data;
     });
     </script>';
-
-  //.load("index.php?etage=1&amp;arrivee='.$_GET['arrivee'].'$amp;depart=" + res);
-
 ?>
