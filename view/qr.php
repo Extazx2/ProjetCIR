82f0d55bf4076abccfd1570dff75e65134cc1474
Extazx2 <touin.marcantoine@gmail.com>
2016-06-07 17:28:02 +0200

    fix on qrcode

--- view/qr.php
+++ view/qr.php
@@ -8,10 +8,29 @@
 		<div class="container">
 			<div id="reader" class="col s12">
 			</div>
+			<h6 class="center">Result</h6>
+			<span id="read" class="center"></span>
+			<br>
+
+			<h6 class="center">Read Error (Debug only)</h6>
+			<span class="center">Will constantly show a message, can be ignored</span>
+			<span id="read_error" class="center"></span>
+
+			<br>
+			<h6 class="center">Video Error</h6>
+			<span id="vid_error" class="center"></span>
 		</div>
 
-    <script> $(\'#reader\').html5_qrcode(function(data){
-         location.href = "index.php?etage=1&amp;arrivee='.$_GET['arrivee'].'$amp;depart=" + data;
-    });
-    </script>';
+    <script>
+    $(document).ready(function(){
+		$(\'#reader\').html5_qrcode(function(data){
+			$(\'#read\').html(data);
+			document.location.href = "index.php?etage=1&arrivee='.$_GET['arrivee'].'&depart=" + data;
+		},
+		function(error){
+			$(\'#read_error\').html(error);
+		}, function(videoError){
+			$(\'#vid_error\').html(videoError);
+		});
+	});</script>';
 ?>
